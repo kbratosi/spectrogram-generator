563ac63d73d94eeeab48d019a49cc5f79df725b0
Konrad Bratosiewicz <konrad.bratosiewicz@gmail.com>
2021-01-04 11:52:20 +0100

    Added more parameters to config.py. Working reading parameters from config.py.

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 # change decoded raw data sample rate
 out_sample_rate = 44100
+
+# Fourier's Transform input packet size
+fft_input_size = 4096
+
+# interlacing
+fft_interlacing = 0.5
+
+# output spectrogram image dimensions
+img_width = 1024
+img_height = 768
--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,44 @@
 #include <boost/python.hpp>
 #include <limits>
 
+namespace bp = boost::python;
+
 int main()
 {
   std::cout << "Hello world!" << std::endl;
-  int out_sample_rate = -1;
+  uint out_sample_rate = 0,
+       img_width = 0,
+       img_height = 0,
+       fft_input_size = 0;
+  float fft_interlacing = 0;
   // Spooky scary skeleton!
   // *GUI
   // get input
 
   // initialize configuration script
   Py_Initialize();
-  boost::python::object config_module = boost::python::import("config");
-  boost::python::object config_namespace = config_module.attr("__dict__");
+  bp::object config_module = bp::import("config");
+  bp::object config_namespace = config_module.attr("__dict__");
   try {
-    out_sample_rate = boost::python::extract<int>(config_namespace["out_sample_rate"]);
+    out_sample_rate = bp::extract<int  >(config_namespace["out_sample_rate"]);
+    img_width       = bp::extract<int  >(config_namespace["img_width"      ]);
+    img_height      = bp::extract<int  >(config_namespace["img_height"     ]);
+    fft_input_size  = bp::extract<int  >(config_namespace["fft_input_size" ]);
+    fft_interlacing = bp::extract<float>(config_namespace["fft_interlacing"]);
   }
-  catch(boost::python::error_already_set const &) {
+  catch(bp::error_already_set const &) {
     PyErr_Print();
+    return -1;
   }
   //
-  std::cout << out_sample_rate << std::endl;
+  std::cout <<   "out_sample_rate: " << out_sample_rate
+            << "\nimg_width:       " << img_width
+            << "\nimg_height:      " << img_height
+            << "\nfft_input_size:  " << fft_input_size
+            << "\nfft_interlacing: " << fft_interlacing 
+            << std::endl;
   int inputSamples = 4096;
   // validate parameters
-  
   /* uncomment later!
   SpectrogramGenerator generator(out_sample_rate, inputSamples);
   std::cout << "Decoder" << std::endl;
