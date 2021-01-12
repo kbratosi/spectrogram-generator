02c8574abadad9f49ff265a8c62d56c880972252
ProtegerM <kobolte@gmail.com>
2021-01-12 22:10:42 +0100

    update unit test

--- test/test.cpp
+++ test/test.cpp
@@ -13,44 +13,63 @@ namespace utf = boost::unit_test;
 
 BOOST_AUTO_TEST_CASE(generate_image)
 {
-    GeneratorConfiguration config;
-    config.fft_out_frame_count_ = 2049;
-    config.img_fft_per_img_ = 500;
-    config.out_sample_rate_ = 44100;
-    config.img_time_per_img_ = 30000;
-    config.img_scale_time_interval_ = 500;
-    config.img_height_ = 900;
-    config.img_width_ = 1600;
 
-    ImageGenerator *image = new ImageGenerator(&config);
-    cv::Mat *testImg = new cv::Mat();
+  GeneratorConfiguration config;
+  config.fft_out_frame_count_ = 2049;
+  config.img_fft_per_img_ = 500;
+  config.out_sample_rate_ = 44100;
+  config.img_time_per_img_ = 30000;
+  config.img_scale_time_interval_ = 500;
+  config.img_height_ = 900;
+  config.img_width_ = 1600;
 
-    BOOST_CHECK_EQUAL(image->getOutputImage()->size(), testImg->size());
+  ImageGenerator *image = new ImageGenerator(&config);
 
-    image->saveImage("1");
+  BOOST_CHECK(image->getTempImage());
+  BOOST_CHECK(image->getImage());
+  BOOST_CHECK(image->getOutputImage());
 
-    BOOST_CHECK(image->getImage()->size() != image->getOutputImage()->size());
+  BOOST_CHECK(image->getTempImage()->at<uint16_t>(1, 1) == 0);
+  BOOST_CHECK(image->getImage()->at<uint16_t>(1, 1) == 0);
+
+  float *tempBuf = new float[2049];
+  std::fill_n(tempBuf, 2049, 20);
+
+  std::vector<float *> transforms;
+  for (int i = 0; i < 5; ++i)
+  {
+    transforms.push_back(tempBuf);
+  }
+
+  image->generateSpectrograms(&transforms);
+
+  BOOST_CHECK(image->getTempImage()->at<uint16_t>(1, 1) > 0);
+  BOOST_CHECK(image->getImage()->at<uint16_t>(1, 1) > 0);
+  BOOST_CHECK(image->getOutputImage()->at<uint16_t>(19, 1) > 0);
+
+  BOOST_CHECK(image->getImage()->size().height != image->getOutputImage()->size().height);
+  BOOST_CHECK(image->getImage()->size().width != image->getOutputImage()->size().width);
 }
 
 BOOST_AUTO_TEST_CASE(process_data)
 {
-    const int SIZE_OF_CHUNK = 10000;
-    GeneratorConfiguration config;
-    config.fft_out_frame_count_ = 2049;
-    config.fft_in_frame_count_ = 4096;
-    config.fft_delta_frame_ = 0.4 * config.fft_in_frame_count_;
+  const int SIZE_OF_CHUNK = 10000;
+  GeneratorConfiguration config;
+  config.fft_out_frame_count_ = 2049;
+  config.fft_in_frame_count_ = 4096;
+  config.fft_delta_frame_ = 0.4 * config.fft_in_frame_count_;
 
-    DataComputer *transformation = new DataComputer(&config);
+  DataComputer *transformation = new DataComputer(&config);
 
-    BOOST_CHECK_EQUAL(transformation->getPtrTransforms()->size(), 0);
+  BOOST_CHECK_EQUAL(transformation->getPtrTransforms()->size(), 0);
 
-    SampleFormat data[SIZE_OF_CHUNK];
-    std::fill_n(data, SIZE_OF_CHUNK, 1);
-    transformation->processSamples(data, SIZE_OF_CHUNK);
-    BOOST_CHECK(transformation->getPtrTransforms()->size() > 0);
+  SampleFormat data[SIZE_OF_CHUNK];
+  std::fill_n(data, SIZE_OF_CHUNK, 1);
 
-    int num_of_FFT = 1 + (SIZE_OF_CHUNK - config.fft_in_frame_count_) / config.fft_delta_frame_;
-    BOOST_CHECK_EQUAL(transformation->getPtrTransforms()->size(), num_of_FFT);
+  transformation->processSamples(data, SIZE_OF_CHUNK);
+
+  int num_of_FFT = 1 + (SIZE_OF_CHUNK - config.fft_in_frame_count_) / config.fft_delta_frame_;
+  BOOST_CHECK_EQUAL(transformation->getPtrTransforms()->size(), num_of_FFT);
 }
 
 BOOST_AUTO_TEST_CASE(validate_parameters)
